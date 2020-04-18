e3f05810cfb01e58b72276ad8699e3cd13114e8c
Annie Phan <anniephan96@gmail.com>
2020-04-18 07:05:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# HW 1
+
+## Due date: Sunday, April 19th before midnight. Submissions only accepted on GitHub.
+
+If you have difficulty using GitHub to commit your code, please ask for help. If you are unsure if your code has committed successfully through Desktop, you can look at the GitHub website to verify. You are entirely responsible for correctly submitting your assignment, and any submissions after the due date will incur a penalty.
+
+  - Go to the syllabus and read the section on Academic Integrity before doing this assignment.
+  - Please write your code in the space below each question, and make sure that your code runs.
+  - Read the questions carefully and present the results in the format asked (e.g. print statements)
+  - If a method is specified, you must use that method to get the result even if you find/know of another way to do it.
+
+Please put your name at the top of your file!  
---
+++ homework 1.py
@@ -0,0 +1,49 @@
+# PPHA 30535
+# Spring 2020
+# Homework 1
+
+# YOUR NAME HERE
+
+# Go to the syllabus and read the section on Academic Integrity before doing this assignment.
+# Please write your code in the space below each question, and make sure that your code runs.
+# Read the questions carefully and present the results in the format asked (e.g. print statements)
+# If a method is specified, you must use that method to get the result even if you find/know of
+# another way to do it.
+
+# Due date: Sunday, April 19th before midnight. Submissions only accepted on GitHub.
+
+#############################################
+
+# Question 1: The Pythagorean theorem says that the sides of a right triangle follow the formula:
+# a squared + b squared = c squared
+# where c is the hypotenuse. Write code that takes in the variables a and b, then prints out the
+# length of the hyptenuse, the area of the triangle (one half base times height) and the perimeter
+# (the sum of all three sides)
+
+
+
+# Question 2: For any given rectangle, the area is simply its base times its height, and its perimeter
+# is the sum of all four sides. Create a dictionary where the keys are a, b, area, and permieter. Fill
+# in any values for a and b, and have the dictionary values for area and permiter calculated.
+
+
+
+# Question 3: A palindrome is a word or phrase that is the same both forwards and backwards. Write code
+# that takes a variable of any string, then tests to see whether it qualifies as a palindrome. Make sure
+# it works on the word "radar" and the phrase "A man, a plan, a canal, Panama!", while rejecting the
+# word "Apple" and the phrase "This isn't a palindrome". Print the result.
+
+
+
+# Question 4: Using a for loop, write code that takes in any list of objects, then prints out:
+# "The value at position __ is __."
+# for every element in the loop, where the first blank is the index location and the second blank is
+# the object at that index location.
+
+
+
+# Question 5: Create a list containing the values 10, 2, 3, 3, and 5, in that order. Then, using a list
+# comprehension, reverse the list and add each element together with the value at the same position in the
+# original list. For example, if a list contains 1 and 3, the reversed version contains 3 and 1. Then
+# summing the first elements gives you 1+3=4, and the second elements gives you 3+1=4, for a final list of
+# 4 and 4. Finally, remove duplicate values and print the result.
